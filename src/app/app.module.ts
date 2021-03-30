ed0aad0c898f9ab298a5222a487b9956a41ceedf
Leo <takib77@gmail.com>
2021-03-30 15:40:27 +0200

    Components, services, classes, npm i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { QuizEditorComponent } from './pages/quiz-editor/quiz-editor.component';
+import { QuestionEditorComponent } from './pages/question-editor/question-editor.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    QuizEditorComponent,
+    QuestionEditorComponent,
+    QuizComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
